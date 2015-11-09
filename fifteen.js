ed964bf43dd1846374238c8bef7505015e51f03e
chrisannerowe <chrisannerowe@gmail.com>
2015-11-08 23:52:53 -0500

    Create fifteen.js

---
+++ fifteen.js
@@ -0,0 +1,17 @@
+var tiles= [];  //array of tiles
+var div= 1;     
+var x=0, y=400;
+
+for(i=0;i<15;i++) {
+    tiles[i]= fifteen.getElementById("puzzlearea").getElementByTagName('div');
+    div++;
+    style.backgroundPosition((x,y));
+    x-= 100;
+    y-= 100;
+}
+
+<button onmouseover='getElementById("div").innerHTML=moveablepiece()'></button>
+
+<button onclick='getElementById("shuffle").innerHTML=move()'></button>
+
+<button onclick='getElementById("div").innerHTML=moveablepiece()'></button>
